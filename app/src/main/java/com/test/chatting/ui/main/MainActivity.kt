e181646b70c7b609c4c9168df9b1e8dd854a3eab
juseong <wjdwntjd55@naver.com>
2023-11-02 13:24:34 +0900

    Feat : MainActivity 에 replaceFragment, removeFragment 기능 구현

---
+++ app/src/main/java/com/test/chatting/ui/main/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.test.chatting.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.test.chatting.R
+import com.test.chatting.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var activityMainBinding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+    }
+
+    fun replaceFragment(name:String, addToBackStack:Boolean, bundle:Bundle?){
+
+        // Fragment 교체 상태로 설정
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+
+        var newFragment: Fragment? = null
+
+        // 새로운 Fragment를 담을 변수
+        newFragment = when(name){
+            else -> Fragment()
+        }
+
+        newFragment?.arguments = bundle
+
+        if(newFragment != null) {
+
+            // Fragment를 교채한다.
+            fragmentTransaction.replace(R.id.fragmentContainerView_main, newFragment)
+
+            if (addToBackStack == true) {
+                // Fragment를 Backstack에 넣어 이전으로 돌아가는 기능이 동작할 수 있도록 한다.
+                fragmentTransaction.addToBackStack(name)
+            }
+
+            // 교체 명령이 동작하도록 한다.
+            fragmentTransaction.commit()
+        }
+    }
+
+    // Fragment를 BackStack에서 제거한다.
+    fun removeFragment(name: String){
+        supportFragmentManager.popBackStack(name, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+    }
+
+}
