720922f4630d1cb6e7c8c5dd878aa1a34ec8fc15
juseong <wjdwntjd55@naver.com>
2023-11-30 17:21:59 +0900

    Feat : 알림 권한 처리 구현

--- app/src/main/java/com/test/chatting/ui/main/MainActivity.kt
+++ app/src/main/java/com/test/chatting/ui/main/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.test.chatting.ui.main
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.test.chatting.R
@@ -26,6 +33,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(activityMainBinding.root)
 
         replaceFragment(LOGIN_FRAGMENT, false, null)
+        askNotificationPermission()
 
     }
 
@@ -117,6 +125,44 @@ class MainActivity : AppCompatActivity() {
     // 어떤, 바텀 네비게이션이 선택되었는지 알려주기
     fun selectBottomNavigationItem(itemId: Int) {
         activityMainBinding.bottomNavigationViewMain.selectedItemId = itemId
+    }
+
+    // Declare the launcher at the top of your Activity/Fragment:
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission(),
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            // FCM SDK (and your app) can post notifications.
+        } else {
+            // 알람권한 없음
+        }
+    }
+
+    private fun askNotificationPermission() {
+        // This is only necessary for API level >= 33 (TIRAMISU)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) ==
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                // FCM SDK (and your app) can post notifications.
+            } else if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
+                showPermissionRationalDialog()
+            } else {
+                // Directly ask for the permission
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun showPermissionRationalDialog() {
+        AlertDialog.Builder(this)
+            .setMessage("알림 권한이 없으면 알림을 받을 수 없습니다.")
+            .setPositiveButton("권한 허용하기") {_, _ ->
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
+            .setNegativeButton("취소") { dialogInterface, _ -> dialogInterface.cancel()}
+            .show()
     }
 
     companion object {
