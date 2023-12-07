f1d5965c83cdafab9c9a9aa24d11c1276f766aee
juseong <wjdwntjd55@naver.com>
2023-12-07 13:00:15 +0900

    Feat : 내 정보에서 프로필 사진 선택하는 기능 구현

--- app/src/main/java/com/test/chatting/ui/mypage/MyPageFragment.kt
+++ app/src/main/java/com/test/chatting/ui/mypage/MyPageFragment.kt
@@ -1,11 +1,14 @@
 package com.test.chatting.ui.mypage
 
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.lifecycle.ViewModelProvider
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
@@ -26,6 +29,16 @@ class MyPageFragment : Fragment() {
 
     val TAG = "MyPageFragment"
 
+    val pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+        // Callback is invoked after th user selects a media item or closes the photo picker.
+        if (uri != null) {
+            binding.imageViewMyPageUserProfile.setImageURI(uri)
+            Log.d(TAG, "Selected URI: $uri")
+        } else {
+            Log.d(TAG, "No media selected")
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -38,6 +51,7 @@ class MyPageFragment : Fragment() {
         viewModel.getCurrentLoginUserInfo(CURRENT_USER_UID)
 
         inintView()
+        updateUserProfile()
         updateUserInfo()
         signOut()
 
@@ -54,6 +68,14 @@ class MyPageFragment : Fragment() {
 
             binding.editTextMyPageDescription.setText(userInfo.description)
 
+        }
+
+    }
+
+    fun updateUserProfile() {
+
+        binding.imageViewMyPageUserProfile.setOnClickListener {
+            pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
         }
 
     }
