043507f37e282b22768dbf315ff9140f56f80846
juseong <wjdwntjd55@naver.com>
2023-12-09 23:00:03 +0900

    Rename : item_chatting -> item_chatting_other 으로 이름 변경

--- app/src/main/java/com/test/chatting/ui/chattinglist/ChattingAdapter.kt
+++ app/src/main/java/com/test/chatting/ui/chattinglist/ChattingAdapter.kt
@@ -8,12 +8,12 @@ import android.widget.TextView
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.test.chatting.databinding.ItemChattingBinding
+import com.test.chatting.databinding.ItemChattingOtherBinding
 import com.test.chatting.model.ChattingItem
 import com.test.chatting.model.User
 
 class ChattingAdapter(val otherUserItem: User, private var allChattingItemList : MutableList<ChattingItem>): RecyclerView.Adapter<ChattingAdapter.ChattingViewHolder>() {
-    inner class ChattingViewHolder(binding: ItemChattingBinding): RecyclerView.ViewHolder(binding.root) {
+    inner class ChattingViewHolder(binding: ItemChattingOtherBinding): RecyclerView.ViewHolder(binding.root) {
 
         val userProfile: ImageView
         val userName: TextView
@@ -28,7 +28,7 @@ class ChattingAdapter(val otherUserItem: User, private var allChattingItemList :
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChattingViewHolder {
-        val binding = ItemChattingBinding.inflate(LayoutInflater.from(parent.context))
+        val binding = ItemChattingOtherBinding.inflate(LayoutInflater.from(parent.context))
         val chattingViewHolder = ChattingViewHolder(binding)
 
         binding.root.layoutParams = ViewGroup.LayoutParams(
